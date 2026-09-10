98a14c2622f173b9a8ffcac44a4178cf707bdfe5
G-Lifshitz <goshalif17@gmail.com>
2026-09-10 11:24:13 +0200

    tax added

--- store.py
+++ store.py
@@ -5,7 +5,7 @@ TODO(team): implement the pricing rule(s) assigned to you in the README.
 
 
 def calculate_total(
-    subtotal, apply_discount=False, apply_tax=True, apply_shipping=False
+    subtotal, apply_discount=False, apply_tax=False, apply_shipping=False
 ):
     """Calculate the final total a customer pays for their cart."""
     total = subtotal
